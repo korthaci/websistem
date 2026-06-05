b19ffbd9e5be302ea0ba62a967fa9a4a29973025
korthaci <korthaci@gmail.com>
2026-06-05 17:02:32 +0300

    Improve template file selection logic for contact form

--- php/modul/iletisim_formu/index.php
+++ php/modul/iletisim_formu/index.php
@@ -1,7 +1,8 @@
 <?php if (! defined('otoban')) exit('!vad');
 
-$modul_sablon_yaz = new sablon_yaz;
-$modul_sablon_yaz->dosya_icerik(__DIR__ . '/sablon/iletisim_formu.html');
-echo $modul_sablon_yaz->render();
+$sablon1 = __DIR__ . '/sablon/'.$so_->d('tema').'/iletisim_formu.html';
+$sablon2 = __DIR__ . '/sablon/iletisim_formu.html';
 
-?>
+$modul_sablon_yaz = new sablon_yaz;
+$modul_sablon_yaz->dosya_icerik(file_exists($sablon1) ? $sablon1 : $sablon2);
+echo $modul_sablon_yaz->render();
